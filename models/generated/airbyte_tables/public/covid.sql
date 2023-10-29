2833791ec5d4310b48da0ec02454f63915ff6803
Ziad Mansor <ziad@gmail.com>
2023-10-29 13:45:11 +0300

    first commit5

--- models/generated/airbyte_tables/public/covid.sql
+++ models/generated/airbyte_tables/public/covid.sql
@@ -24,7 +24,7 @@
 -- depends_on: {{ ref('covid_ab3') }}
 select
     {{ adapter.quote('date') }},
-    new_recovered as new_recovered_zizo ,
+    new_recovered,
     new_tested,
     total_deceased,
     new_deceased,
